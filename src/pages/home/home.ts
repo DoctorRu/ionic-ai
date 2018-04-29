e131c5cec95263114fd45844a0c5555bdaa8c27c
DoctorRu <vjblan@gmail.com>
2018-04-28 23:27:57 -0300

    UI: Css: chat dialog

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
-
+   
+   messages: any[] = [];
+   
+   constructor(public navCtrl: NavController) {
+       
+       this.messages.push({
+           text: "Hi, how can I help you?",
+           sender: "api"
+       });
+    
+       this.messages.push({
+           text: "Hello",
+           sender: "me"
+       });
+    }
+    
 }
